9d9b16eeb7b6c81da52759e17d01f7d7d8d17fcc
Chris Imgarten <cjimgarten@gmail.com>
2017-08-08 19:32:32 -0500

    add files

--- src/main/java/com/cjimgarten/dummy/controller/DummyApiController.java
+++ src/main/java/com/cjimgarten/dummy/controller/DummyApiController.java
@@ -1,6 +1,8 @@
 package com.cjimgarten.dummy.controller;
 
-import com.cjimgarten.dummy.model.Dummy;
+import com.cjimgarten.dummy.model.data.DummyDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "dummy")
 public class DummyApiController {
 
-    @RequestMapping(value = "")
-    public Dummy index() {
-        return new Dummy();
+    @Autowired
+    private DummyDao dao;
+
+    @RequestMapping(value = "findAll")
+    public Object findAll() {
+        return dao.findAll();
+    }
+
+    @RequestMapping(value = "findById/{id}")
+    public Object findById(@PathVariable int id) {
+        return dao.findOne(id);
     }
 }
--- src/main/java/com/cjimgarten/dummy/model/Dummy.java
+++ src/main/java/com/cjimgarten/dummy/model/Dummy.java
@@ -1,13 +1,35 @@
 package com.cjimgarten.dummy.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Created by chris on 8/3/17.
  */
+@Entity
 public class Dummy {
 
+    @Id
+    @GeneratedValue
+    private int id;
     private String firstName;
     private String lastName;
     private String dob;
+
+    public Dummy() {
+
+    }
+
+    public Dummy(String firstName, String lastName, String dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+    }
+
+    public int getId() {
+        return id;
+    }
 
     public String getFirstName() {
         return firstName;
