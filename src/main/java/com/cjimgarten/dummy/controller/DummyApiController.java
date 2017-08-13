bfcbd4cddb7474f69e14c9eb92b4ca896d54407a
Chris Imgarten <cjimgarten@gmail.com>
2017-08-13 10:49:39 -0500

    modify controller

--- src/main/java/com/cjimgarten/dummy/controller/DummyApiController.java
+++ src/main/java/com/cjimgarten/dummy/controller/DummyApiController.java
@@ -1,10 +1,9 @@
 package com.cjimgarten.dummy.controller;
 
 import com.cjimgarten.dummy.model.data.DummyDao;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by chris on 8/3/17.
@@ -16,13 +15,24 @@ public class DummyApiController {
     @Autowired
     private DummyDao dao;
 
-    @RequestMapping(value = "findAll")
+    @RequestMapping(value = "findAll", method = RequestMethod.GET)
     public Object findAll() {
         return dao.findAll();
     }
 
-    @RequestMapping(value = "findById/{id}")
-    public Object findById(@PathVariable int id) {
+    @RequestMapping(value = "findById/{id}", method = RequestMethod.GET)
+    public Object findByIdPathVariable(@PathVariable int id) {
+        return dao.findOne(id);
+    }
+
+    @RequestMapping(value = "findByIdRequest", method = RequestMethod.GET)
+    public Object findByIdRequest(HttpServletRequest request) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        return dao.findOne(id);
+    }
+
+    @RequestMapping(value = "findByIdParam", method = RequestMethod.GET)
+    public Object findByIdParam(@RequestParam(value = "id") int id) {
         return dao.findOne(id);
     }
 }
